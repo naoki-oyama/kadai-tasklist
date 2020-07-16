12071a95fdf639da9610217b13c90870e2377ed2
naoki.oyama <oyama14@c.kyoai.ac.jp>
2020-07-16 19:56:52 +0900

    Add getAllMessages

--- src/controllers/indexT.java
+++ src/controllers/indexT.java
@@ -1,12 +1,17 @@
 package controllers;
 
 import java.io.IOException;
+import java.util.List;
 
+import javax.persistence.EntityManager;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import models.messageT;
+import utils.DBUtilT;
 
 /**
  * Servlet implementation class indexT
@@ -28,8 +33,12 @@ public class indexT extends HttpServlet {
      */
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        // TODO Auto-generated method stub
-        response.getWriter().append("Served at: ").append(request.getContextPath());
-    }
+        EntityManager em = DBUtilT.createEntityManager();
 
+        List<messageT> messages = em.createNamedQuery("getAllMessages", messageT.class)
+                .getResultList();
+        response.getWriter().append(Integer.valueOf(messages.size()).toString());
+
+        em.close();
+    }
 }
